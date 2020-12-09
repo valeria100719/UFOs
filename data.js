4a5b9889a4f7f3e509da4efb371f808a3ea4c18a
valeria100719 <71678830+valeria100719@users.noreply.github.com>
2020-12-09 10:29:38 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+var data = [
+    {
+      datetime: "1/1/2010",
+      city: "benton",
+      state: "ar",
+      country: "us",
+      shape: "circle",
+      durationMinutes: "5 mins.",
+      comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+    },
